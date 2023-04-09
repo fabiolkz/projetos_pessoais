20a612a984e1ea035332411440ab92609eb89e3a
fabiolkz <fabiodosantoslk@gmail.com>
2023-04-09 16:30:49 -0300

    Continuando projeto

---
+++ urna_eletrônica/script.js
@@ -0,0 +1,100 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+ 
+/* VARIAVEIS DE CONTROLE */
+
+let etapaAtual = 0; 
+let numero = '';
+
+const comecarEtapa = () => {
+
+    let etapa = etapas[etapaAtual];
+
+    let numeroHTML = '';
+    numero = '';
+
+    for( let i = 0; i < etapa.numeros; i++ ) { 
+        if ( i === 0 ) {
+            numeroHTML += '<div class="numero pisca"></div>'
+        } else {
+            numeroHTML += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHTML;
+}
+
+const atualizaInterface = () => { 
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) => {
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Pardido: ${candidato.partido}`;
+
+        let fotosHTML = '';
+        for(let i in candidato.fotos) { 
+            fotosHTML += `<div class='d-1-image'><img src='images/${candidato.fotos[i].url}' alt=''>${candidato.fotos[i].legenda}</div>`
+        }
+    
+        lateral.innerHTML = fotosHTML;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.style.display = 'flex';
+        descricao.style.alignItems = 'center';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO NULO</div>';
+    }
+
+    console.log('Candidato', candidato);
+
+}
+
+const clicou = (n) => {
+    let elNumero = document.querySelector('.numero.pisca');
+
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if (elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
+
+    }
+
+}
+
+const branco = () => {
+
+}
+
+const corrige = () => {
+    comecarEtapa();
+}
+
+const confirma = () => {
+
+}
+
+comecarEtapa();
